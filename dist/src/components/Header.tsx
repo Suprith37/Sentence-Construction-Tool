fd977b5d61743920223e4eddfaf9816a40ba567e
Suprith R.Y <109782151+Suprith37@users.noreply.github.com>
2025-04-14 17:17:20 +0530

    Add files via upload

---
+++ dist/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export function Header(){
+    return (
+    <div className="heading_cont absolute top-0 left-0 h-16 w-full max flex items-center justify-center shadow-[0_2px_36px_0_rgba(0,0,0,0.08)] backdrop-blur-[50px]">
+            <span className="font-['Poppins'] text-[20px] font-semibold text-[rgba(0,0,0,0.46)]">
+                Sentence Construction
+            </span>
+    </div>
+    )
+}
